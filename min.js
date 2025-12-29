2cbf41560f21c7e09b0948be892a0fca6286a747
hai07t <hai07t@gmail.com>
2025-12-29 23:12:49 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,20 +1,20 @@
-(function () {
-  const ua = navigator.userAgent || "";
-  const isAndroid = /Android/i.test(ua);
-  const isWebView = /\bwv\b|Android.*Version\/[\d.]+.*Chrome\/[\d.]+/i.test(ua);
-  const isMobile = /Android|iPhone|iPad|iPod/i.test(ua);
+// (function () {
+//   const ua = navigator.userAgent || "";
+//   const isAndroid = /Android/i.test(ua);
+//   const isWebView = /\bwv\b|Android.*Version\/[\d.]+.*Chrome\/[\d.]+/i.test(ua);
+//   const isMobile = /Android|iPhone|iPad|iPod/i.test(ua);
 
-  if (!isAndroid || !isWebView || !isMobile) return;
+//   if (!isAndroid || !isWebView || !isMobile) return;
 
-  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const language = navigator.language || "";
+//   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+//   const language = navigator.language || "";
 
-  const isVietnam =
-    timeZone === "Asia/Ho_Chi_Minh" ||
-    language.toLowerCase().startsWith("vi");
+//   const isVietnam =
+//     timeZone === "Asia/Ho_Chi_Minh" ||
+//     language.toLowerCase().startsWith("vi");
 
-  if (isVietnam && !sessionStorage.getItem("vn_redirected")) {
-    sessionStorage.setItem("vn_redirected", "1");
-    window.location.replace("https://www.google.com/");
-  }
-})();
+//   if (isVietnam && !sessionStorage.getItem("vn_redirected")) {
+//     sessionStorage.setItem("vn_redirected", "1");
+//     window.location.replace("https://www.google.com/");
+//   }
+// })();
